fb1e10a0e81a2e1e52202cec7e29864ecf96a2eb
saitaruns <saitarunsst@gmail.com>
2023-01-25 11:15:20 +0530

    finished

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector(".setting-btn").
-    addEventListener("click", () => {
-        document.getElementById("sub-side-panel").classList.toggle("active")
-    })
+// document.querySelector(".setting-btn").
+//     addEventListener("click", () => {
+//         document.getElementById("sub-side-panel").classList.toggle("active")
+//     })
 // // document.querySelector(".setting-btn").addEventListener("mouseleave", () => {
 // //     document.getElementById("sub-side-panel").classList.remove("active")
 // })
